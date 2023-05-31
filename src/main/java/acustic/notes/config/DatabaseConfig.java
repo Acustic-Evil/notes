c2fe460b7f5b7a9810378c681f2a3dea51eed09d
Acustic_Evil <prisha931@gmail.com>
2023-05-31 03:43:15 +0300

    changed dependencies, application properties and added DatabaseConfig for jdbc

---
+++ src/main/java/acustic/notes/config/DatabaseConfig.java
@@ -0,0 +1,25 @@
+package acustic.notes.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/notes");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("1234");
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        return dataSource;
+    }
+
+    // Other configuration beans and methods...
+}
+
