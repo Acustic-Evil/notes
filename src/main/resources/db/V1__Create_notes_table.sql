4e095f78edf85b65105fabe82a8f94de2b36c23e
Acustic_Evil <prisha931@gmail.com>
2023-05-31 14:27:26 +0300

    still trying to avoid conflict of versions

--- src/main/resources/db/V1__Create_notes_table.sql
+++ src/main/resources/db/V1__Create_notes_table.sql
@@ -1,6 +1,6 @@
 CREATE SEQUENCE IF NOT EXISTS notes_id_seq;
 CREATE TABLE IF NOT EXISTS notes (
-    id SERIAL PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     title varchar(255),
     text text
 );
